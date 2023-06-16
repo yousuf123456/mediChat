4d15fb076cb19bdf27e1458a1c0edbe6449cc494
yousuf123456 <muhammadyousuf554456@gmail.com>
2023-06-16 21:28:29 +0500

    AlittleMore

--- app/libs/beamsClient.ts
+++
@@ -1,8 +0,0 @@
-import * as PusherPushNotifications from "@pusher/push-notifications-web";
-
-import PushNotifications from "@pusher/push-notifications-server";
-
-export const beamsClient = new PushNotifications({
-    instanceId : "9b399d99-0617-4a35-b047-d496fc3e201b",
-    secretKey : "57C0592EDEE7C4DB2E020C0AB7523F1FAD97021368365A6EFAA0E05684CBFD4C"
-})
