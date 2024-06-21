b00dea50696909d96e1b0ae9378fe2edbcb01111
yousuf123456 <muhammadyousuf554456@gmail.com>
2024-06-21 12:23:42 +0500

    Fix 2

--- app/actions/getSession.ts
+++
@@ -1,9 +0,0 @@
-import { getServerSession } from "next-auth";
-
-import { authOptions } from "../api/auth/[...nextauth]/route";
-
-export async function getSession(){
-    const session = await getServerSession(authOptions);
-
-    return session
-}
